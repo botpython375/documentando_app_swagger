3ea64b335e264cd84fd60c16de12f67a8d93ef9e
botpython375 <botpython375@gmail.com>
2025-06-09 21:37:48 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,3 +6,31 @@ def test_home():
     response = client.get("/")
     assert response.status_code == 200
     assert response.get_json() == {"message": "API está funcionando"}
+
+
+def test_login_sucesso():
+    client = app.test_client()
+    response = client.post(
+        "/login", json={"email": "user@example.com", "senha": "1234"}
+    )
+    assert response.status_code == 200
+    assert "access_token" in response.get_json()
+
+
+def test_protegido_sem_token():
+    client = app.test_client()
+    response = client.get("/protegido")
+    assert response.status_code == 401
+
+
+def test_protegido_com_token():
+    client = app.test_client()
+
+    # Primeiro faz login
+    login = client.post("/login", json={"email": "user@example.com", "senha": "1234"})
+    token = login.get_json()["access_token"]
+
+    # Acessa rota protegida com o token
+    response = client.get("/protegido", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+    assert "Olá, user@example.com!" in response.get_json()["msg"]
